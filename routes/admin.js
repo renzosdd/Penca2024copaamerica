d54eb8abc4581bdddef7bb646ac791dcced7a6c5
Renzo Dogliotti <renzodogliotti@gmail.com>
2024-06-21 10:27:55 -0300

    Admin page

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,27 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Servir la página de administración
-router.get('/edit', isAuthenticated, isAdmin, (req, res) => {
-    res.render('admin', { user: req.session.user });
+router.get('/edit', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const users = await User.find().select('username');
+        res.render('admin', { user: req.session.user, users });
+    } catch (error) {
+        res.status(500).send('Error al cargar la página de administración');
+    }
+});
+
+// Endpoint para obtener los datos de un usuario específico
+router.get('/user/:username', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username }).select('-password');
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error al obtener los datos del usuario:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 // Endpoint para actualizar el perfil del usuario
