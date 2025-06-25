c30659bc9491a196adb174711e2d279bd06cbb01
Renzo Dogliotti <44270754+renzosdd@users.noreply.github.com>
2025-06-25 17:07:50 -0300

    feat: track competition in pencas

--- models/Penca.js
+++ models/Penca.js
@@ -4,6 +4,7 @@ const pencaSchema = new mongoose.Schema({
   name: { type: String, required: true },
   code: { type: String, required: true, unique: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  competition: { type: String, required: true },
   participantLimit: { type: Number, default: 20 },
   fixture: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Match' }],
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
